ffd4b2cf5037001718457d4a71ec2d59e9bb6990
Pierre Depaz <pierre.depaz@gmail.com>
2019-02-23 15:20:49 +0100

    added controller code without shifters

--- code/controller.py
+++ code/controller.py
@@ -8,154 +8,108 @@ import threading
 
 GPIO.setmode(GPIO.BCM)
 ## 1
-CLOCK_1 = 18 #green
-DATA_1 = 23 #blue
-LATCH_1 = 24 #orange
+MOTOR_A_A = 18
+MOTOR_A_B = 23
+MOTOR_A_C = 24
+MOTOR_A_D = 25
 
-CLOCK_2 = 6 #orange
-DATA_2 = 25 #purple
-LATCH_2 = 12 #yellow
+MOTOR_B_A = 4
+MOTOR_B_B = 17
+MOTOR_B_C = 27
+MOTOR_B_D = 22
 
-CLOCK_3 = 26 #white
-DATA_3 = 16 #grey
-LATCH_3 = 20 #white
+MOTOR_C_A = 12
+MOTOR_C_B = 16
+MOTOR_C_C = 20
+MOTOR_C_D = 21
 
-forward_first = [
-    10, # 1010,
-    6,  # 0110,
-    5,  # 0101,
-    9   # 1001
-]
-forward_second = [160, 96, 80, 144]
+MOTOR_D_A = 6
+MOTOR_D_B = 13
+MOTOR_D_C = 19
+MOTOR_D_D = 26
 
-backward_first = [
-    9, # 1001,
-    5, # 0101,
-    6, # 0110,
-    10 # 1010
-]
-backward_second = [144, 80, 96, 160]
+
 
 class Motor:
-    def __init__(self, _id, _neighbour, _clock, _latch, _data):
+    def __init__(self, _id, _a, _b, _c, _d):
         self.id = _id
-        self.neighbour = _neighbour
-        self.clock_pin = _clock
-        self.latch_pin = _latch
-        self.data_pin = _data
-        if _id == "A" or _id == "C" or _id == "E":
-            self.first = True
-        else:
-            self.first = False
+        self.pins = [_a, _b, _c, _d]
 
-        if(self.first):
-            self.fwd = [10, 6, 5, 9]
-            self.bwd = [9, 5, 6, 10]
-        else:
-            self.fwd = [160, 96, 80, 144]
-            self.bwd = [144, 80, 96, 160]
-
-    def clock(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.output(self.clock_pin, 1)
-        time.sleep(.0001)
-        GPIO.output(self.clock_pin, 0)
-
-    def latch(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.output(self.latch_pin, 1)
-        time.sleep(.0001)
-        GPIO.output(self.latch_pin, 0)
-
-    def writePin(self, value):
-        for x in range(0, 8):
-            temp = value & 0x80
-            if temp == 0x80:
-                GPIO.output(self.data_pin, 1)
-            else:
-                GPIO.output(self.data_pin, 0)
-            self.clock()
-            value = value << 0x01
-        self.latch()
+    def setStep(self, values):
+        GPIO.output(self.pins[0], values[0])
+        GPIO.output(self.pins[1], values[1])
+        GPIO.output(self.pins[2], values[2])
+        GPIO.output(self.pins[3], values[3])
 
     def up(self, delay, steps):
-        GPIO.setmode(GPIO.BCM)
         for i in range(0, steps):
-            self.writePin(self.fwd[0])
+            setStep([1, 0, 1, 0])
             time.sleep(delay)
-            self.writePin(self.fwd[1])
+            setStep([0, 1, 1, 0])
             time.sleep(delay)
-            self.writePin(self.fwd[2])
+            setStep([0, 1, 0, 1])
             time.sleep(delay)
-            self.writePin(self.fwd[3])
-            
-        self.reset()
+            setStep([1, 0, 0, 1])
+            time.sleep(delay)
+
 
     def down(self, delay, steps):
         for i in range(0, steps):
-            self.writePin(self.bwd[0])
+            setStep([1, 0, 0, 1])
             time.sleep(delay)
-            self.writePin(self.bwd[1])
+            setStep([0, 1, 0, 1])
             time.sleep(delay)
-            self.writePin(self.bwd[2])
+            setStep([0, 1, 1, 0])
             time.sleep(delay)
-            self.writePin(self.bwd[3])
-        self.reset()
+            setStep([1, 0, 1, 0])
+            time.sleep(delay)
 
     def move(self, dir, delay, step):
         if dir == "up":
-            t = threading.Thread(target=self.up, args=(delay, step))
-            t.start()
+            self.up(delay, steps)
         else:
-            t = threading.Thread(target=self.down, args=(delay, step))
-            t.start()
+            self.down(delay, steps)
 
     def reset(self):
-        for state in [0, 0, 0, 0]:
-            self.writePin(state)
+        self.setStep(0, 0, 0, 0)
 
 motors = []
 
-motors.append(Motor("A", "B", CLOCK_1, LATCH_1, DATA_1))
-motors.append(Motor("B", "A", CLOCK_1, LATCH_1, DATA_1))
-motors.append(Motor("C", "D", CLOCK_2, LATCH_2, DATA_2))
-motors.append(Motor("D", "C", CLOCK_2, LATCH_2, DATA_2))
-motors.append(Motor("E", "F", CLOCK_3, LATCH_3, DATA_3))
-motors.append(Motor("F", "E", CLOCK_3, LATCH_3, DATA_3))
+motors.append(Motor("A", MOTOR_A_A, MOTOR_A_B, MOTOR_A_C, MOTOR_A_D))
+motors.append(Motor("B", MOTOR_B_A, MOTOR_B_B, MOTOR_B_C, MOTOR_B_D))
+motors.append(Motor("C", MOTOR_C_A, MOTOR_C_B, MOTOR_C_C, MOTOR_C_D))
+motors.append(Motor("D", MOTOR_D_A, MOTOR_D_B, MOTOR_D_C, MOTOR_D_D))
 
-GPIO.setup(CLOCK_1, GPIO.OUT)
-GPIO.setup(DATA_1, GPIO.OUT)
-GPIO.setup(LATCH_1, GPIO.OUT)
+GPIO.setup(MOTOR_A_A, GPIO.OUT)
+GPIO.setup(MOTOR_A_B, GPIO.OUT)
+GPIO.setup(MOTOR_A_C, GPIO.OUT)
+GPIO.setup(MOTOR_A_D, GPIO.OUT)
 
-GPIO.setup(CLOCK_2, GPIO.OUT)
-GPIO.setup(DATA_2, GPIO.OUT)
-GPIO.setup(LATCH_2, GPIO.OUT)
+GPIO.setup(MOTOR_B_A, GPIO.OUT)
+GPIO.setup(MOTOR_B_B, GPIO.OUT)
+GPIO.setup(MOTOR_B_C, GPIO.OUT)
+GPIO.setup(MOTOR_B_D, GPIO.OUT)
 
-GPIO.setup(CLOCK_3, GPIO.OUT)
-GPIO.setup(DATA_3, GPIO.OUT)
-GPIO.setup(LATCH_3, GPIO.OUT)
+GPIO.setup(MOTOR_C_A, GPIO.OUT)
+GPIO.setup(MOTOR_C_B, GPIO.OUT)
+GPIO.setup(MOTOR_C_C, GPIO.OUT)
+GPIO.setup(MOTOR_C_D, GPIO.OUT)
 
-GPIO.output(LATCH_1, 0) #we close the latch
-GPIO.output(LATCH_2, 0)
-GPIO.output(LATCH_3, 0)
-GPIO.output(CLOCK_1, 0) #idk/
-GPIO.output(CLOCK_2, 0)
-GPIO.output(CLOCK_3, 0)
+GPIO.setup(MOTOR_D_A, GPIO.OUT)
+GPIO.setup(MOTOR_D_B, GPIO.OUT)
+GPIO.setup(MOTOR_D_C, GPIO.OUT)
+GPIO.setup(MOTOR_D_D, GPIO.OUT)
 
 def main():
     try:
         for motor in motors:
             motor.reset()
-        #motors[4].move("up", 0.195, 40)
-        motors[5].move("down", 0.175, 40)
-        for t in threading.enumerate():
-            try:
-                t.join()
-            except RuntimeError as err:
-                if 'cannot join current thread' in err:
-                    continue
-                else:
-                    raise
+
+        motors[0].move("up", 0.175, 40)
+        motors[1].move("up", 0.125, 40)
+        motors[2].move("up", 0.075, 40)
+        motors[3].move("up", 0.225, 40)
+
         GPIO.cleanup()
 
     except KeyboardInterrupt:
